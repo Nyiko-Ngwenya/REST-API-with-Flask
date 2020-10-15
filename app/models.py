121e90649ce15801fe8534517f862abd1ad41b91
Nyiko <nyikongwenya@gmail.com>
2020-10-15 15:53:57 +0200

    Added gitignore  , added routes , config files

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    trait = db.Column(db.String(120))
+    cost = db.Column(db.Integer)
+    
+
+
+    def __repr__(self):
+        return '<Character {}>'.format(self.name) 
