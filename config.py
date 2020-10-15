483a1f6ad541c0cae8a3a8a0d14246534ecce61e
Nyiko <nyikongwenya@gmail.com>
2020-10-15 15:59:55 +0200

    Made the config point to the postgres server

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pguser:password@localhost:6543/production'#os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pguser:password@localhost:6543/LoLDb'#os.environ.get('DATABASE_URL') or \
         # 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
